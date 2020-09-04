a47d0c361c0c9e0349f83a194cdb66f1cad2fa12
thompsonjn2 <70284272+thompsonjn2@users.noreply.github.com>
2020-09-04 10:19:17 -0400

    hw code update

--- Homework/Homework.py
+++ Homework/Homework.py
@@ -1,3 +1,16 @@
 #Jasmine
 #This is Homework 1
 
+import logging
+logging.basicConfig(level=logging.DEBUG)
+def squared_threes():
+
+    return_value = []
+    numbers = range(3,102,3)
+    for value in numbers:
+        logging.debug(value ** 2)
+ return return_value
+ 
+if __name__ == "__main__":
+ for x in squared_threes():
+ print(x)
