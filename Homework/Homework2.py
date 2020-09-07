6458339408fe92385992955151ecdce9460d2db6
thompsonjn2 <70284272+thompsonjn2@users.noreply.github.com>
2020-09-07 14:49:59 -0400

    commit

---
+++ Homework/Homework2.py
@@ -0,0 +1,37 @@
+#5-8 Hello Admin:
+
+usernames = ['David','John','Tricia', 'Ashley', 'admin']
+
+for username in usernames:
+    if username =='admin':
+        print ("Hello admin, would you like to see a status report?")
+    else:
+        print("Hello " + username + ", thank you for logging in again!")
+
+
+#5-9 No Users:
+usernames = []
+
+if usernames:
+    for username in usernames:
+        if username == 'admin':
+            print ("Hello admin, would you like to see a status report?")
+        else:
+            print("Hello " + username + ", thank you for logging in again!")
+else:
+    print('We need to find some users!')
+
+#5-10 Checking Usernames
+
+current_users = ['janet', 'kayla', 'JAY', 'lucy', 'bill']
+new_users = ['lily', 'jay', 'thomas', 'kayla', 'patrick']
+lower_current_users = []
+for user in current_users:
+    lower_current_users.append(user.lower())
+
+for new_user in new_users:
+    if new_user.lower in lower_current_users:
+        print('Sorry ' + new_user + ', that username is taken.')
+    else:
+        print('Ok ' + new_user + 'that username is available!')
+
