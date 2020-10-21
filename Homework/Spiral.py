83d18b410400e72bbc7cc8474eca078b0befef62
thompsonjn2 <70284272+thompsonjn2@users.noreply.github.com>
2020-10-21 16:58:44 -0400

    hw update

---
+++ Homework/Spiral.py
@@ -0,0 +1,18 @@
+def main():
+    counter = 0
+    gap = 1
+    four_counter = 4
+    total = 0
+    for n in range(0,1001**2):
+        if counter == 0:
+            total += n
+            counter = gap
+            four_counter -= 1
+        elif counter != 0: 
+            counter -=1
+        if four_counter == 0:
+            gap +=2
+            four_counter = 4
+    if __name__ == "__main__":
+        print(total)
+
